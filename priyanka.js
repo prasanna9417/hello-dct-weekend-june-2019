f93b3bcf9e0a85faa9a5a93dddf77551066ab14b
Priyanka Jacob <priyanka.jacob93@gmail.com>
2019-10-05 14:03:04 +0530

    Made changes to the file Priyanka.js

--- priyanka.js
+++ priyanka.js
@@ -1,1 +1,10 @@
 // This is created by priyanka
+//hello world
+function greeting(name){
+    console.log("welcome",name)
+}
+
+greeting("Sony")
+greeting("Tony")
+greeting("Mony")
+greeting("Kony")
