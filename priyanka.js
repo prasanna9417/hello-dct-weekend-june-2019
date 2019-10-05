dee291ba9d0ea294c27b1d4f8c34a4e43e109f99
Prasanna Balaji <prasanna9417@gmail.com>
2019-10-05 14:03:22 +0530

    changes made by prasanna

--- priyanka.js
+++ priyanka.js
@@ -1,1 +1,17 @@
 // This is created by priyanka
+
+def hello():
+  print("Hello World") 
+  return("hello")
+
+
+# Define `plus()`
+def plus(a,b):
+  sum = a + b
+  return (sum, a)
+
+# Call `plus()` and unpack variables 
+sum, a = plus(3,4)
+
+# Print `sum()`
+print(sum)
